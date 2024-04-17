6400e2711d6456d2e8e36d9ba0b3b76f23900605
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-17 21:45:03 +0600

    added decode token , save session storage, navigate to page after sucess login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 declare var google: any;
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit{
   
-
+private router = inject(Router);
 
   ngOnInit(): void {
     google.accounts.id.initialize({
@@ -39,7 +39,17 @@ export class LoginComponent implements OnInit{
        */
 
       client_id: '531121168764-fj0qmf042utb4q601id0dt2ojmdpe6ej.apps.googleusercontent.com',
+
       callback: (resp: any)=> {
+        /**
+         * We will get client_id, credential from this response
+         * if we copy the credential and go to jwt.io
+         * we can decript the token where we will get all the information what we need like user name , user image and so on.
+         */
+        console.log(resp);
+
+        // From where it will be decripted
+        this.handleLogin(resp);
 
       }
     });
@@ -50,6 +60,31 @@ export class LoginComponent implements OnInit{
       shape: 'rectangle',
       width: 350
     })
+  }
+
+  private decodeToken(token: string){
+    /**
+     * What JWT token contain ?
+     * First is Header
+     * Secound is Credential or Payload
+     * Third is Secret or key
+     * 
+     * So we are interested in the credential or payload only 
+     * So we are taking the secound thing by spliting.
+     */
+    return JSON.parse(atob(token.split(".")[1]));
+  }
+
+  handleLogin(response : any){
+    if(response){
+       //decode the token
+       const payLoad = this.decodeToken(response.credential);
+       //store in session
+       sessionStorage.setItem("loggedInUser", JSON.stringify(payLoad));
+       //navigate to home/browse
+       this.router.navigate(['browse'])
+    }
+
   }
 
 }
