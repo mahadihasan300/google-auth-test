8865b16d6a8308821873eeae7846cb4e54a0c91c
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-17 22:35:11 +0600

    auth service added for signout , also added remove session storage after signout

---
+++ src/app/auth.service.ts
@@ -0,0 +1,21 @@
+declare var google: any;
+
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  router = inject(Router);
+
+  constructor() { }
+
+  signOut(){
+    // from docomentation developers.google.com
+    google.accounts.id.disableAutoSelect();
+    this.router.navigate(['/']);
+
+  }
+}
--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-browse',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './browse.component.css'
 })
 export class BrowseComponent {
+
+  auth = inject(AuthService);
+
+  signOut(){
+    /**
+     * As we are storing data in sessin storage 
+     * so whenever we signout we need to clear that from session storage.
+     * we are getting the key name loggedInUser fron Application -> Session Storage (If we inspact we can see that)
+     */
+    sessionStorage.removeItem("loggedInUser")
+
+    this.auth.signOut();
+  }
 
 }
