b1de60b073236140d438c3ddbd0614ff21392adf
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-17 21:18:56 +0600

    google sigin in button added without functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+declare var google: any;
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  
+
+
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id: '',
+      callback: (resp: any)=> {
+
+      }
+    });
+
+    google.accounts.id.renderButton(document.getElementById("google-btn"), {
+      theme: 'filled_blue',
+      size: 'large',
+      shape: 'rectangle',
+      width: 350
+    })
+  }
 
 }
