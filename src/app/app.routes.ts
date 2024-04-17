9e00a699126dc9854f0cd8ead62ad94e10d8bbe9
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-17 22:15:08 +0600

    routung brows page works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { BrowseComponent } from './browse/browse.component';
 
 export const routes: Routes = [
-    {path: '','title' : 'Log In', component: LoginComponent}
+    {path: '','title' : 'Log In', component: LoginComponent},
+    {path: 'browse','title' : 'Browse', component: BrowseComponent}
 ];
