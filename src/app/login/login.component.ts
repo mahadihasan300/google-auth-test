a2b6e66cbbba3a72a5830150b620f58e66a83cfd
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-04-17 21:29:48 +0600

    client id aded and able to show all the gmail account while clicking the buttn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,7 +15,30 @@ export class LoginComponent implements OnInit{
 
   ngOnInit(): void {
     google.accounts.id.initialize({
-      client_id: '',
+      /***
+       * To generate client-id we need to go to
+       * console.cloud.google.com
+       * Then
+       *    Create a new project
+       *    select the project
+       *    API and Services
+       *      Then
+       *        OAuth consent screen
+       *          Extarnal
+       *          Give app-name, domain.
+       *          In scope select all info what i need
+       *          Save.
+       *        Credential
+       *          Create Credentials 
+       *          OAuth client Id
+       *          Application type = web application
+       *          Authorised JavaScript origins:
+       *            Add Uri : http://localhost:4200/
+       *            Domain : http://localhost
+       *            Creat.
+       */
+
+      client_id: '531121168764-fj0qmf042utb4q601id0dt2ojmdpe6ej.apps.googleusercontent.com',
       callback: (resp: any)=> {
 
       }
